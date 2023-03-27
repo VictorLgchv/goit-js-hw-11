18c75e9fe472b29c6c809a6c3418cf5e91f8649c
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-03-27 14:14:04 +0300

    Update fetchImages.js

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '34737052-b22dcc77d23fc25c00a98711a';
 async function fetchImages(serachArray, page) {
-  const resp = await fetch(`${BASE_URL}/?key=${API_KEY}&q=${serachArray}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=4`);
+  const resp = await fetch(`${BASE_URL}/?key=${API_KEY}&q=${serachArray}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
   const newCountry = await resp.json();
   return newCountry;
 }
